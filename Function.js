6f5fed86648cd9979e9c81c5f39c43d5eeb7ddaa
shraimp <92940248+shraimp@users.noreply.github.com>
2021-12-09 23:20:09 +0100

    Add files via upload

---
+++ Function.js
@@ -0,0 +1,57 @@
+var student = {
+    Fitst_name: "Mohammad",
+    Last_name : "Shraim",
+   Faculty : "ME",
+    Drop : function(crs){}
+};
+Public class Switch_Case_Example {
+
+    int day = 50; 
+    string time = "Morning"; 
+
+    switch (day) {
+        case 1;
+          FileSystem.out.println("Monday");
+          break;
+    case 2:
+          FileSystem.out.println("Tuesday");
+          break;
+    case 3:
+            FileSystem.out.println("wednesday");
+            break;
+    case 4:
+          FileSystem.out.println("Thursday");
+          break;
+    case 5:
+          FileSystem.out.println("Friday");
+          break;
+    case 5:
+          FileSystem.out.println("Saturday");
+          break;
+    case 6:
+          FileSystem.out.println("Sunday");
+          switch(time){
+              case "Morning":
+                  FileSystem.out.println("Morning");
+                  break;
+              case "Afternoon":
+                  FileSystem.out.println("Afternoon") 
+                  break;   
+          } 
+          break;
+          default:
+              FileSystem.out.println("Please Provide proper day number");   
+        }         
+    }
+}
+FunctionDriving; 
+
+var age; 
+if (age >= 18); 
+ FileSystem.out.println("you can drive");
+
+ else 
+ FileSystem.out.println("You can not drive");
+
+             
+}
